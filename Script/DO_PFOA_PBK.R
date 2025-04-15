e943477cfdec6d969b401ba72da1aef58fd0b552
Chrysanthi Pachoulide <chrysanthi.pachoulide@wur.nl>
2025-04-15 09:08:37 +0200

    Small typo

--- Script/DO_PFOA_PBK.R
+++ Script/DO_PFOA_PBK.R
@@ -26,15 +26,15 @@
   
   source(here("Script", "RUN_and_OUTPUT.R"))
   
-  RESULTS <- RUNandOUT(exposure_type = "Oral", # type of exposure
+  RESULTS <- RUNandOUT(exposure_type = exposure_type, # type of exposure
                        expCONC = 0.048, # ug/kg/day
                        Tinput = 1, # for repeated exposure or so, default = 1
                        tinterval = 1, # for repeated exposure or so, default = 1
                        expAGE = 70, # age at exposure, needed to select physiological parameters
-                       expSTOP = 1, # time in days after which the exposure stopped 
+                       expSTOP = 20*356, # time in days after which the exposure stopped 
                        Tstart = 0, # days, start of the simulation
-                       Tstop = 2*356, # days, stop of the simulation
-                       Dt = 1/10 # days, iteration steps (decrease/increase depending on run time)
+                       Tstop = 50*356, # days, stop of the simulation
+                       Dt = 1 # days, iteration steps (decrease/increase depending on run time)
                        )
 
   save(RESULTS, file = here(OUTPUT, "RESULTS_PFOA_PBK.RData"))
