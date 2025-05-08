ba51a4e84880e46d63444465227e2077a35eebc5
Chrysanthi Pachoulide <chrysanthi.pachoulide@wur.nl>
2025-05-08 13:04:11 +0200

    changed expCONC to exp in all relevant files; checked that GFR calculation using BH and BSA works, it works; this is now the option in the files

--- Script/DO_PFOA_PBK.R
+++ Script/DO_PFOA_PBK.R
@@ -21,7 +21,7 @@
   dir.create(OUTPUT, recursive = TRUE)
   
   # Choose if physiology should change with age ("Yes" to include physiological changes due to age and "No" to assume the same physiology over time)
-  Lifestage = "Yes" 
+  Lifestage = "No" 
   
   # Choose to include population or individual exposure ("Yes" to include population based and "No" to only run the model for one person)
   Population = "Yes" 
@@ -38,12 +38,14 @@
   if(Population == "Yes"){
     
     INPUT_dummy <- read.csv(here("Input", "INPUT_dummy.csv")) 
+    Input <- INPUT_dummy %>% filter(Idcode %in% 1:3) # Only include for Olsen data
+    
     
     if(Lifestage == "Yes" && any(is.na(INPUT_dummy$expAGE))){
       
       warning("Removing ", sum(is.na(INPUT_dummy$expAGE)), " samples as they had NA(s) as expAGE; exposure AGE is needed to run the lifestage model")
       Input <- filter (INPUT_dummy, !is.na(expAGE))
-      Input <- Input %>% filter(Idcode %in% 1:26)
+      Input <- Input %>% filter(Idcode %in% 1:3) # Only include Olsen data
     } 
     
     nPeople <- as.numeric(nrow(Input)) # number of people
@@ -64,9 +66,9 @@
     
     # Exposure-relevant information
     exposure_type = exposure_type # type of exposure
-    expCONC_Oral = 0 # ug/kg/day concentration to be used only when both oral and dermal are used
-    expCONC_Dermal = 1E-3 # ug/kg/day concentration to be used only when both oral and dermal are used
-    expCONC = expCONC_Oral + expCONC_Dermal # ug/kg/day concentration
+    exp_Oral = 0 # ug/kg/day concentration to be used only when both oral and dermal are used
+    exp_Dermal = 1E-3 # ug/kg/day concentration to be used only when both oral and dermal are used
+    exp = exp_Oral + exp_Dermal # ug/kg/day concentration
     Tinput = 1 # for repeated exposure or so default = 1
     tinterval = 1 # for repeated exposure or so default = 1
     expSTOP = 27*365 # time in days after which the exposure stopped
@@ -114,11 +116,11 @@
         Pop.MODEL_OUTPUT <- RUNandOUT_lifestage(
           
           exposure_type = as.character(Input[i, "exposure_type"]),            # Exposure type, choose between "Oral", "Dermal", "Oral_Dermal" (if exposure is both Oral and Dermal), Inhalation"
-          expCONC = as.numeric(Input[i, "expCONC"]),                           # ug/kg/day concentration (total exposure concentration)
-          expCONC_Oral = ifelse(is.na(Input[i, "expCONC_Oral"]), 0,
-                                 as.numeric(Input[i, "expCONC_Oral"])),        # ug/kg/day concentration to be used only when both oral and dermal are used
-          expCONC_Dermal = ifelse(is.na(Input[i, "expCONC_Dermal"]), 0,
-                                   as.numeric(Input[i, "expCONC_Dermal"])),    # ug/kg/day concentration to be used only when both oral and dermal are used
+          exp = as.numeric(Input[i, "exp"]),                           # ug/kg/day concentration (total exposure concentration)
+          exp_Oral = ifelse(is.na(Input[i, "exp_Oral"]), 0,
+                                 as.numeric(Input[i, "exp_Oral"])),        # ug/kg/day concentration to be used only when both oral and dermal are used
+          exp_Dermal = ifelse(is.na(Input[i, "exp_Dermal"]), 0,
+                                   as.numeric(Input[i, "exp_Dermal"])),    # ug/kg/day concentration to be used only when both oral and dermal are used
           Tinput = ifelse(is.na(Input[i, "Tinput"]), 1,
                            as.numeric(Input[i, "Tinput"])),          # for repeated exposure or so default = 1
           tinterval = ifelse(is.na(Input[i, "tinterval"]), 1,
@@ -161,11 +163,11 @@
         Pop.MODEL_OUTPUT <- RUNandOUT(
           
           exposure_type = as.character(Input[i, "exposure_type"]),            # Exposure type, choose between "Oral", "Dermal", "Oral_Dermal" (if exposure is both Oral and Dermal), Inhalation"
-          expCONC = as.numeric(Input[i, "expCONC"]),                           # ug/kg/day concentration (total exposure concentration)
-          expCONC_Oral = ifelse(is.na(Input[i, "expCONC_Oral"]), 0,
-                                 as.numeric(Input[i, "expCONC_Oral"])),        # ug/kg/day concentration to be used only when both oral and dermal are used
-          expCONC_Dermal = ifelse(is.na(Input[i, "expCONC_Dermal"]), 0,
-                                   as.numeric(Input[i, "expCONC_Dermal"])),    # ug/kg/day concentration to be used only when both oral and dermal are used
+          exp = as.numeric(Input[i, "exp"]),                           # ug/kg/day concentration (total exposure concentration)
+          exp_Oral = ifelse(is.na(Input[i, "exp_Oral"]), 0,
+                                 as.numeric(Input[i, "exp_Oral"])),        # ug/kg/day concentration to be used only when both oral and dermal are used
+          exp_Dermal = ifelse(is.na(Input[i, "exp_Dermal"]), 0,
+                                   as.numeric(Input[i, "exp_Dermal"])),    # ug/kg/day concentration to be used only when both oral and dermal are used
           Tinput = ifelse(is.na(Input[i, "Tinput"]), 1,
                            as.numeric(Input[i, "Tinput"])),          # for repeated exposure or so default = 1
           tinterval = ifelse(is.na(Input[i, "tinterval"]), 1,
@@ -206,9 +208,9 @@
         
         # Exposure-relevant information
         exposure_type = exposure_type, 
-        expCONC = expCONC, 
-        expCONC_Oral = expCONC_Oral, 
-        expCONC_Dermal = expCONC_Dermal, 
+        exp = exp, 
+        exp_Oral = exp_Oral, 
+        exp_Dermal = exp_Dermal, 
         Tinput = Tinput, 
         tinterval = tinterval, 
         expSTOP = expSTOP, 
@@ -231,9 +233,9 @@
         
         # Exposure-relevant information
         exposure_type = exposure_type, 
-        expCONC = expCONC, 
-        expCONC_Oral = expCONC_Oral, 
-        expCONC_Dermal = expCONC_Dermal, 
+        exp = exp, 
+        exp_Oral = exp_Oral, 
+        exp_Dermal = exp_Dermal, 
         Tinput = Tinput, 
         tinterval = tinterval, 
         expSTOP = expSTOP, 
